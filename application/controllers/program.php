a53147ca6826f90f05dbc2d2e3096bcced4fe5e8
Izzan Oktiadi <zand.only@gmail.com>
2017-03-07 11:13:27 +0700

    Sudah dengan perhitäungan kuantitatif

--- application/controllers/program.php
+++ application/controllers/program.php
@@ -236,7 +236,7 @@ class Program extends CI_Controller {
 	
 	public function input_data_segment(){
 		//$segment = $this->uri->segment(3);
-    	$exel = $this->read_excel("Program.xlsx");
+    	$exel = $this->read_excel("workblok.xlsx");
     	$arrres = array(); $s=0;
     	//if($this->mnasabah->empty_table('nasabah')){
 		for ($row = 2; $row <= $exel['row']; ++$row) {
@@ -245,7 +245,7 @@ class Program extends CI_Controller {
 				$arrres[$row][$col] = $exel['wrksheet']->getCellByColumnAndRow($col, $row)->getValue();
 			}
 			
-			//Program
+			/*Program
 			
 			$data['category'] = $arrres[$row][0];
 			$data['segment'] = $arrres[$row][1];
@@ -256,22 +256,23 @@ class Program extends CI_Controller {
 			$data['pmo_head'] = $arrres[$row][6];
 			$data['sort'] = $arrres[$row][7];
 			
-			$this->mprogram->insert_program($data); 
+			$this->mprogram->insert_program($data); */
 			
 
-			/* Initiative
+			/*Initiative
 			$data['title'] = $arrres[$row][0];
 			$data['program_id'] = $arrres[$row][1];
-			$this->minitiative->insert_initiative($data);
-			*/
+			$data['start'] = date("Y-m-d",$this->excelDateToDate($arrres[$row][2]));
+			$data['end'] = date("Y-m-d",$this->excelDateToDate($arrres[$row][3]));
+			$this->minitiative->insert_initiative($data);*/
 			
 			//Workblock
-			/*
 			$data['title'] = $arrres[$row][0];
 			$data['initiative_id'] = $arrres[$row][3];
 			$data['start'] = date("Y-m-d",$this->excelDateToDate($arrres[$row][1]));
 			$data['end'] = date("Y-m-d",$this->excelDateToDate($arrres[$row][2]));
-			$this->mworkblock->insert_workblock($data);	*/
+			$data['code'] = $arrres[$row][4];
+			$this->mworkblock->insert_workblock($data);
 		}
     }
 
--- application/views/program/component/_list_of_program.php
+++ application/views/program/component/_list_of_program.php
@@ -14,11 +14,6 @@
 		</tr>
 	</thead>
 	<tbody>
-		<?php
-			/*usort($programs, function($a, $b) {
-				return $a['code'] - $b['code'];
-			});*/
-		?>
 		<?php 
 		$segment=""; $i=1; $segnum=1;
 		$pv_init = ""; $total_percent="";
@@ -28,8 +23,10 @@
 			<td style="width:400px">
 				<?php if($pv_init != $prog['prog']->segment){?>
 					<div style="float:left; width:50px; margin-right:5px;"><?php echo $prog['prog']->init_code?>
-
-						<?php if($prog['total']!=null) {echo (number_format($prog['total'], 2, ',', ' '));}else{}?></div>
+					</div>
+					<div>
+					<?php if($prog['total']!=null && $prog['tot_kual']!=null && isset($prog['tot_kual'])){echo 'Total Kuantitatif & Kualitatif: '.(number_format((($prog['total']+$prog['tot_kual'])/2), 2, ',', ' ')).'%';}else{}?>
+					</div>
 					<b><div style="float:left; max-width:300px"><?php echo $prog['prog']->segment?></div></b>
 					<div style="clear:both"></div>
 					<b><div style="float:left; max-width:300px; margin-top:10px">Direktur Sponsor: <?php echo $prog['prog']->dir_spon?></div></b>
@@ -44,7 +41,7 @@
 				<div style="clear:both"></div>
 			</td>
 			<td>
-				<div style="float:left; width:50px; margin-right:15px;"><?php if(isset($prog['lu']->last_update)) echo $prog['lu']->last_update?></div> 
+				<div style="float:left; width:50px; margin-right:15px;"><?php if(isset($prog['lu']->last_update)) echo date("j F Y G:i:s",strtotime($prog['lu']->last_update));?></div> 
 				<div style="float:left; width:50px; margin-right:15px;">
 				</div>
 			</td>
@@ -58,33 +55,11 @@
 			<span style="font-size:14px; color:<?php echo color_status($prog['init_status'])?>; font-weight:bold"><?php echo $prog['init_status']?></span>
 			<?php } }?> 
 			</td>
-			<td><?php if($prog['wb_total']==0){"0";}else{ echo ($prog['wb_status']['complete']/$prog['wb_total'])*100;}?>% jumlah status in progress:<?php echo $prog['status']['In Progress'] ?> jumlah deliverable:<?php echo $prog['sub_init_total']?> total workblock complete:<?php echo $prog['wb_status']['complete']?>  total workblock dalam deliverable:<?php echo $prog['wb_total']?></td>
+			<td><?php if($prog['wb_total']==0){"0";}else{ echo(($prog['wb_status']['complete']/$prog['wb_total'])*100);}?>%</td>
 			<?php if($user['role']=='admin'){?><td style="width:50px">
 				<button class="btn btn-warning  btn-xs" onclick="show_form(<?php echo $prog['prog']->id?>);"><span class="glyphicon glyphicon-pencil"></span></button>
 				<button class="btn btn-danger btn-xs" onclick="delete_program(<?php echo $prog['prog']->id?>)"><span class="glyphicon glyphicon-trash"></span></button>
 			</td><?php }?>
-		</tr>
-		<tr id="edit_prog_<?php echo $prog['prog']->id?>" style="display:none">
-			<div>
-			<td colspan=9>
-				<form class="form-horizontal" action="<?php echo base_url();?>initiative/submit_initiative/<?php echo $prog['prog']->id?>" method ="post" id="formsignup" role="form">
-					<div class="form-group">
-						<label class="col-sm-2 control-label">Program</label>
-						<div class="col-sm-4">
-							<input type="text" class="form-control" id="title" name="title" placeholder="Initiative" value="<?php echo $prog['prog']->title?>">
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">Id</label>
-						<div class="col-sm-4">
-							<input type="text" class="form-control" name="code" id="code" placeholder="Id" value="<?php echo $prog['prog']->code?>">
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="" class="col-sm-2 control-label"></label>
-						<div class="col-sm-4"><input type="submit" class="btn btn-success"></div></div>
-				</form>
-			</div>
 		</tr>
 		<?php $i++;}?>
 	</tbody>
