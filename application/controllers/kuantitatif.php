52b7b0932f12562ad6baafdde19d649ee5d195c1
fabrora <fabrora@outlook.com>
2017-03-14 16:04:04 +0700

    Kuantitatif

---
+++ application/controllers/kuantitatif.php
@@ -0,0 +1,347 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Kuantitatif extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('minitiative');
+        $this->load->model('mprogram');
+        $this->load->model('mworkblock');
+        $this->load->model('mremark');
+        $this->load->model('mmilestone');
+        $this->load->model('muser');
+        $this->load->model('mfiles_upload');
+        $this->load->model('mkuantitatif');
+        $this->load->model('mkuantitatif_update');
+        $this->load->library('excel');
+
+        $session = $this->session->userdata('user');
+
+        if(!$session){
+            redirect('user/login');
+        }
+    }
+    /**
+     * Method for page (public)
+     */
+    public function index()
+    {
+        redirect('program/list_programs');
+    }
+
+    /*Program*/
+    public function list_programs(){
+        //$segment = $this->uri->segment(3);
+        //if(!$segment){$segment = "Accelerate the growth segment";}
+
+        $data['title'] = "List All Program";
+        //$prog['arr_categ'] = $this->mfiles_upload->get_distinct_col("category","asc","program");
+
+        $prog['page']="all";
+
+        $user = $this->session->userdata('user');
+        $prog['user'] = $user;
+        $pending_aprv = $this->mmilestone->get_pending_aprv($user['id'],$user['role']);
+
+        $prog['programs'] = $this->mprogram->get_segment_programs('','','','');
+        $prog['all_count_wb'] = $this->mworkblock->get_count_workblock();
+        $init = $this->mprogram->get_init_code();
+        //$prog['kuantitatif'] = $this->mprogram->get_kuantitatif_by_init_code($init->init_code);
+        //$data['header'] = $this->load->view('shared/header',array('user' => $user,'pending'=>$pending_aprv),TRUE);
+        $prog['indicator'] = $this->load->view('program/component/_indicator',$prog,TRUE);
+        //$prog['list_program'] = $this->load->view('program/component/_list_of_program',$prog,TRUE);
+        $prog['list_program'] = $this->load->view('program/component/kuantitatif',$prog,TRUE);
+        $data['footer'] = $this->load->view('shared/footer','',TRUE);
+        $data['header'] = $this->load->view('shared/header-new','',TRUE);
+        //$data['sidebar'] = $this->load->view('shared/sidebar_2',$prog,TRUE);
+        $data['content'] = $this->load->view('program/list_program',$prog,TRUE);
+
+        $this->load->view('front',$data);
+    }
+
+    public function summary(){
+        $data['title'] = "List All Program";
+
+        $prog['page']="all";
+
+        $user = $this->session->userdata('user');
+        $prog['user'] = $user;
+        $pending_aprv = $this->mmilestone->get_pending_aprv($user['id'],$user['role']);
+
+        $init_id = 1;
+
+        $views['init'] = $this->minitiative->get_initiative_by_id($init_id);
+        $views['init_status'] = $this->minitiative->get_initiative_status_only($views['init']);
+        $views['wb_status'] = $this->minitiative->get_init_workblocks_status($init_id);
+        $views['info'] = $this->load->view('initiative/detail/_general_info_old',array(
+            'initiative'=>$views['init'],'stat'=>$views['init_status'],'wb' => $views['wb_status']),TRUE);
+
+        //$prog['programs'] = $this->mprogram->get_segment_programs('','','','');
+
+        //$prog['list_program'] = $this->load->view('program/component/_list_of_program',$prog,TRUE);
+
+        $data['footer'] = $this->load->view('shared/footer','',TRUE);
+        $data['sidebar'] = $this->load->view('shared/sidebar_2',$prog,TRUE);
+        $data['content'] = $this->load->view('program/summary',$views,TRUE);
+
+        $this->load->view('front',$data);
+    }
+
+    public function my_inisiatif(){
+        $data['title'] = "My List Program";
+        $prog['arr_categ'] = $this->mfiles_upload->get_distinct_col("category","asc","program");
+
+        $prog['page']="my";
+
+        $user = $this->session->userdata('user');
+        $prog['user'] = $user;
+        $pending_aprv = $this->mmilestone->get_pending_aprv($user['id'],$user['role']);
+        $init_id= explode(";",$this->muser->get_user_by_id($user['id'])->initiative);
+
+        $prog['programs'] = $this->mprogram->get_segment_programs('',$init_id,'','');
+        $prog['indicator'] = $this->load->view('program/component/_indicator',$prog,TRUE);
+        $prog['list_program'] = $this->load->view('program/component/_list_of_kuantitatif',$prog,TRUE);
+
+        $data['header'] = $this->load->view('shared/header-new','',TRUE);
+        $data['footer'] = $this->load->view('shared/footer','',TRUE);
+        // $data['sidebar'] = $this->load->view('shared/sidebar_2',$prog,TRUE);
+        $data['content'] = $this->load->view('program/list_program',$prog,TRUE);
+
+        $this->load->view('front',$data);
+    }
+
+    public function input_program(){
+        $data['title'] = "Input Program";
+
+        $id = $this->input->get('id');
+
+        $user = $this->session->userdata('user');
+        $pending_aprv = $this->mmilestone->get_pending_aprv($user['id'],$user['role']);
+
+
+        if($id){
+            $data['all'] = $this->mprogram->get_program_by_id($id);
+            $data['code']= $this->mprogram->get_program_by_init_code($data['all']->init_code);
+
+        }
+        else{
+            $data['code'] = $this->mfiles_upload->get_distinct_col("code","asc","program");
+        }
+
+        $data['init_code'] = $this->mfiles_upload->get_distinct_col("init_code","asc","program");
+
+        /*$data['arr_segment'] = $this->mfiles_upload->get_distinct_col("segment","asc","program");
+
+
+        $data['category'] = $this->mfiles_upload->get_distinct_col("category","asc","program");
+        $data['dir'] = $this->mfiles_upload->get_distinct_col("dir_spon","asc","program");
+        $data['pmo'] = $this->mfiles_upload->get_distinct_col("pmo_head","asc","program");*/
+
+        //$data['header'] = $this->load->view('shared/header',array('user' => $user,'pending'=>$pending_aprv),TRUE);
+        //$data['footer'] = $this->load->view('shared/footer','',TRUE);
+        //$data['sidebar'] = $this->load->view('shared/sidebar','',TRUE);
+        //$data['content'] = $this->load->view('initiative/input_program',$data,TRUE);
+
+        //$this->load->view('front',$data);
+        $data['all_list']=$this->load->view('initiative/form_all',$data,TRUE);
+
+        $json['html'] = $this->load->view('initiative/input_program',$data,TRUE);
+        $json['status'] = 1;
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($json));
+    }
+
+    public function filter_data(){
+        $user = $this->session->userdata('user');
+        $code = $this->input->get('code_filter');
+
+        if($code=="category"){
+            $data['arr'] = $this->mfiles_upload->get_distinct_col("category","asc","program");
+        }
+
+        if($code=="dir_spon"){
+            $data['arr'] = $this->mfiles_upload->get_distinct_col("dir_spon","asc","program");
+        }
+
+        if($code=="pmo_head"){
+            $data['arr'] = $this->mfiles_upload->get_distinct_col("pmo_head","asc","program");
+        }
+
+        if($code=="co_pmo"){
+            $data['arr'] = $this->muser->get_user_co_pmo();
+            $json['html'] = $this->load->view('program/component/_list_of_co_pmo',$data,TRUE);
+
+        }
+
+        if($code!="co_pmo"){
+            $json['html'] = $this->load->view('program/component/_list_of_filter',$data,TRUE);
+        }
+        $json['status'] = 1;
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($json));
+    }
+
+    public function change_code(){
+        $data['code'] = $this->input->get('init_code');
+        $data['all']= $this->mprogram->get_program_by_init_code($data['code'])[0];
+        $json['html'] = $this->load->view('initiative/form_all',$data,TRUE);
+        $json['status'] = 1;
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($json));
+    }
+
+    public function change_data(){
+        $code = $this->input->get('code_filter');
+        $filter = $this->input->get('filter');
+
+        if($code=="category"){
+            $data['programs'] = $this->mprogram->get_segment_programs($filter,'','','');
+        }
+
+        if($code=="dir_spon"){
+            $data['programs'] = $this->mprogram->get_segment_programs('','',$filter,'');
+        }
+
+        if($code=="pmo_head"){
+            $data['programs'] = $this->mprogram->get_segment_programs('','','',$filter);
+        }
+
+        if($code=="co_pmo"){
+            $data['programs'] = $this->mprogram->get_segment_programs('',$filter,'','');
+        }
+
+        $data['user'] = $this->session->userdata('user');
+
+        $json['html'] = $this->load->view('program/component/_list_of_kuantitatif',$data,TRUE);
+        $json['wb'] = $this->load->view('program/component/_indicator',$data,TRUE);
+        $json['completed'] = $this->load->view('program/component/_completed',$data,TRUE);
+        $json['status'] = 1;
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($json));
+    }
+
+    public function submit_program(){
+        $id = $this->uri->segment(3);
+
+        $program['title'] = $this->input->post('title');
+        $program['code'] = $this->input->post('code');
+        $program['segment'] = $this->input->post('segment');
+        $program['category'] = $this->input->post('category');
+        $program['init_code'] = $this->input->post('init_code');
+        $program['dir_spon'] = $this->input->post('dir_spon');
+        $program['pmo_head'] = $this->input->post('pmo_head');
+
+        if($id){
+            $this->mprogram->update_program($program,$id);
+        }
+        else{$this->minitiative->insert_program($program);}
+
+        redirect('program/list_programs');
+    }
+
+    public function delete_program(){
+        $id = $this->input->post('id');
+        if($this->minitiative->delete_program($id)){
+            $json['status'] = 1;
+        }
+        else{
+            $json['status'] = 0;
+        }
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($json));
+    }
+
+    public function input_data_segment(){
+        //$segment = $this->uri->segment(3);
+        $exel = $this->read_excel("workblok.xlsx");
+        $arrres = array(); $s=0;
+        //if($this->mnasabah->empty_table('nasabah')){
+        for ($row = 2; $row <= $exel['row']; ++$row) {
+            $data = "";
+            for ($col = 0; $col < $exel['col']; ++$col) {
+                $arrres[$row][$col] = $exel['wrksheet']->getCellByColumnAndRow($col, $row)->getValue();
+            }
+
+            /*Program
+
+            $data['category'] = $arrres[$row][0];
+            $data['segment'] = $arrres[$row][1];
+            $data['title'] = $arrres[$row][2];
+            $data['code'] = $arrres[$row][3];
+            $data['init_code'] = $arrres[$row][4];
+            $data['dir_spon'] = $arrres[$row][5];
+            $data['pmo_head'] = $arrres[$row][6];
+            $data['sort'] = $arrres[$row][7];
+
+            $this->mprogram->insert_program($data); */
+
+
+            /*Initiative
+            $data['title'] = $arrres[$row][0];
+            $data['program_id'] = $arrres[$row][1];
+            $data['start'] = date("Y-m-d",$this->excelDateToDate($arrres[$row][2]));
+            $data['end'] = date("Y-m-d",$this->excelDateToDate($arrres[$row][3]));
+            $this->minitiative->insert_initiative($data);*/
+
+            //Workblock
+            $data['title'] = $arrres[$row][0];
+            $data['initiative_id'] = $arrres[$row][3];
+            $data['start'] = date("Y-m-d",$this->excelDateToDate($arrres[$row][1]));
+            $data['end'] = date("Y-m-d",$this->excelDateToDate($arrres[$row][2]));
+            $data['code'] = $arrres[$row][4];
+            $this->mworkblock->insert_workblock($data);
+        }
+    }
+
+    public function input_data_kuantitatif(){
+        $exel = $this->read_excel("Kuantitatif.xlsx");
+        $arrres = array(); $s=0;
+        for ($row = 2; $row <= $exel['row']; ++$row) {
+            $data = "";
+            for ($col = 0; $col < $exel['col']; ++$col) {
+                $arrres[$row][$col] = $exel['wrksheet']->getCellByColumnAndRow($col, $row)->getValue();
+            }
+            $target="2017";
+            //Kuantitatif
+            $data['init_code'] = $arrres[$row][0];
+            $data['title'] = $arrres[$row][1];
+            $data['metric'] = $arrres[$row][2];
+            $data['realisasi'] = $arrres[$row][3];
+            $data['target'] = $arrres[$row][4];
+            $data['real_year'] = $target-1;
+            $data['target_year'] = $target;
+            //date("Y-m-d",$this->excelDateToDate($arrres[$row][1]));
+            //$data['end'] = date("Y-m-d",$this->excelDateToDate($arrres[$row][2]));
+            $this->mkuantitatif->insert_kuantitatif($data);
+        }
+    }
+
+    /*Function PHP EXCEL for parsing*/
+    function read_excel($file){
+        $arrres = array();
+        $objReader = PHPExcel_IOFactory::createReader('Excel2007');
+        $objReader->setReadDataOnly(TRUE);
+        $objPHPExcel = $objReader->load("assets/upload/".$file);
+
+        $arrres['wrksheet'] = $objPHPExcel->getActiveSheet();
+        // Get the highest row and column numbers referenced in the worksheet
+        $arrres['row'] = $arrres['wrksheet']->getHighestRow(); // e.g. 10
+        $highestColumn = $arrres['wrksheet']->getHighestColumn(); // e.g 'F'
+        $arrres['col'] = PHPExcel_Cell::columnIndexFromString($highestColumn);
+
+        return $arrres;
+    }
+
+    function SaveViaTempFile($objWriter){
+        $filePath = '/tmp/' . rand(0, getrandmax()) . rand(0, getrandmax()) . ".tmp";
+        $objWriter->save($filePath);
+        readfile($filePath);
+        unlink($filePath);
+    }
+    function excelDateToDate($readDate){
+        $phpexcepDate = $readDate-25569; //to offset to Unix epoch
+        return strtotime("+$phpexcepDate days", mktime(0,0,0,1,1,1970));
+    }
+
+}
---
+++ application/models/mkuantitatif_update.php
@@ -0,0 +1,230 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of admins
+ *
+ * @author Maulnick
+ */
+class mkuantitatif_update extends CI_Model {
+    //put your code here
+    function __construct() {
+        parent::__construct();
+        $this->load->database();
+        $this->load->model('minitiative');
+    }
+
+    //INSERT or CREATE FUNCTION
+
+
+    function insert_program($program){
+        return $this->db->insert('program', $program);
+    }
+
+    //GET FUNCTION
+
+    function get_program_by_id($id){
+        $this->db->where('id',$id);
+        $result = $this->db->get('program');
+        if($result->num_rows==1){
+            return $result->row(0);
+        }else{
+            return false;
+        }
+    }
+
+    function get_program_by_code($code){
+        $this->db->where('code',$code);
+        $result = $this->db->get('program');
+        if($result->num_rows==1){
+            return $result->row(0);
+        }else{
+            return false;
+        }
+    }
+
+    function get_program_by_init_code($code){
+        $this->db->distinct();
+        $this->db->select('segment, category, dir_spon, pmo_head,init_code');
+        if($code)$this->db->where('init_code',$code);
+        $query = $this->db->get('program');
+        return $query->result();
+    }
+
+    function get_segment_programs($segment,$init_id,$dir_spon,$pmo_head){
+        if($segment){
+            $this->db->where('category', $segment);
+        }
+        if($dir_spon){
+            $this->db->where('dir_spon', $dir_spon);
+        }
+        if($pmo_head){
+            $this->db->where('pmo_head', $pmo_head);
+        }
+        if($init_id){
+            foreach ($init_id as $row) {
+                $this->db->or_where('init_code', $row);
+            }
+        }
+        $this->db->where_in('category',return_all_category());
+        //$this->db->order_by('init_code', 'asc');
+        //$this->db->order_by('code', 'asc');
+        $this->db->select('program.*, initiative.id as init_id');
+        $this->db->join('initiative','initiative.id = program.id');
+        $query = $this->db->get('program');
+        $arr = array(); $i=0;
+        $progs = $query->result();
+        foreach($progs as $prog){
+            $arr[$i]['prog'] = $prog;
+            $code = explode('.',$prog->code);
+            $init_id=$prog->init_id;
+            //$arr[$i]['code'] = ($code[0]*100)+$code[1];
+            //$arr[$i]['date'] = $this->minitiative->get_initiative_minmax_date($prog->id);
+            $arr[$i]['lu'] = $this->minitiative->get_initiative_last_update($prog->id);
+            $arr[$i]['init'] = $this->minitiative->get_initiative_by_id($init_id);
+            $arr[$i]['init_status'] = $this->minitiative->get_status_only_by_prog_id($arr[$i]['init'],$prog->id);
+            $arr[$i]['total'] = $this->mprogram->get_kuantitatif_by_init_code($prog->init_code);
+            $arr[$i]['metric'] = $this->mprogram->get_metric_by_init_code($prog->init_code);
+            $arr[$i]['wb_status'] = $this->minitiative->get_init_workblocks_status_new($prog->id);
+
+            $arr[$i]['status'] = $this->get_program_status($prog->id);
+            $arr[$i]['wb_total']= $this->get_total_wb_by_program($prog->id);
+            $arr[$i]['sub_init_total'] = count($this->minitiative->get_all_program_initiatives($prog->id));
+            //count($this->get_total_wb_by_init_code($prog->init_code));
+            //$arr[$i]['wb_all_status'] = $this->minitiative->get_init_workblocks_status_init_code($prog->init_code);
+
+            //$arr[$i]['kuantitatif']=$this->get_kuantitatif_by_init_code($prog->init_code);
+            $i++;
+        }
+        return $arr;
+    }
+
+    function get_all_programs_with_segment($segment){
+        $this->db->order_by('code', 'asc');
+        if($segment != 'all'){
+            $this->db->where('segment', $segment);
+        }
+        $query = $this->db->get('program');
+        return $query->result();
+    }
+
+    function get_program_status($program_id){
+        $allstat = return_arr_status();
+        $arr_status = array();
+        foreach($allstat as $each){$arr_status[$each]=0;}
+        $this->db->where('program_id', $program_id);
+        $this->db->select('initiative.*, program.segment');
+        $this->db->join('program','initiative.program_id = program.id');
+        $query = $this->db->get('initiative');
+        $inits = $query->result();
+        foreach($inits as $init){
+            $status = $this->minitiative->get_initiative_status_only($init);
+            if(!$status){
+                if($init->status){$status=$init->status;}
+                else{$status = "Not Started Yet";}
+            }
+            $arr_status[$status] = $arr_status[$status]+1;
+        }
+        return $arr_status;
+    }
+
+    function get_total_wb_by_program($program_id){
+        $this->db->where('program_id', $program_id);
+        $this->db->select('initiative.*, program.segment');
+        $this->db->join('program','initiative.program_id = program.id');
+        $query = $this->db->get('initiative');
+        $inits = $query->result();
+        $status=""; $total="";
+        foreach($inits as $init){
+            $status = $this->minitiative->get_total_wb_by_init($init);
+            $total += $status;
+        }
+        return $total;
+    }
+
+    function get_total_wb_by_init_code($init_code){
+        $this->db->where('code', $init_code);
+        $this->db->select('*');
+        //$this->db->join('workblock','workblock.code = program.init_code');
+        $query = $this->db->get('workblock');
+        $inits = $query->result();
+        return $inits;
+    }
+
+    function get_kuantitatif_by_init_code($init_code){
+        $this->db->distinct();
+        $this->db->where('kuantitatif_update.init_code', $init_code);
+        $this->db->select('kuantitatif_update.*');
+        $this->db->join('program','kuantitatif_update.init_code = program.init_code');
+        $query = $this->db->get('kuantitatif_update');
+        $inits = $query->result();
+        $total=0; $temp=0; $i=0; $hasil=0;
+        foreach($inits as $init){
+
+            if($init->realisasi==0 || $init->target==0)
+            {
+                $temp=0;
+            }
+            else{
+                $temp = (($init->realisasi/$init->target)*100);
+            }
+            $hasil += $temp;
+            $i++;
+        }
+        if($i!=0){
+            $hasil = $hasil/$i;
+        }
+        return $hasil;
+    }
+
+    function get_metric_by_init_code($init_code){
+        $this->db->distinct();
+        $this->db->where('kuantitatif_update.init_code', $init_code);
+        $this->db->select('kuantitatif_update.*');
+        $this->db->join('program','kuantitatif_update.init_code = program.init_code');
+        $query = $this->db->get('kuantitatif_update');
+        $inits = $query->result();
+        $hasil="";
+        foreach($inits as $init){
+
+
+            $hasil = $init->metric;
+        }
+
+        return $inits;
+    }
+
+    function get_init_code(){
+        $this->db->distinct();
+        //$this->db->where('kuantitatif.init_code', $init_code);
+        $this->db->select('kuantitatif_update.init_code');
+        $this->db->join('program','kuantitatif_update.init_code = program.init_code');
+        $query = $this->db->get('kuantitatif_update');
+        return $query;
+    }
+    //UPDATE FUNCTION
+
+    function update_program($program,$id){
+        $this->db->where('id',$id);
+        return $this->db->update('program', $program);
+    }
+
+    //DELETE FUNCTION
+    function delete_program(){
+        $id = $this->input->post('id');
+        $this->db->where('id',$id);
+        $this->db->delete('program');
+        if($this->db->affected_rows()>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    // OTHER FUNCTION
+}
