f35357f4da97e38962b377507e5a77f1bf89f855
metafiliana <metafiliana@gmail.com>
2017-12-06 21:17:59 +0700

    fix 2 angka dibelakang koma kuantitatif type 3 summary

--- application/controllers/summary.php
+++ application/controllers/summary.php
@@ -1134,8 +1134,8 @@ class Summary extends CI_Controller {
                     $data_initiative_detail_raw['init_code'] = $value->init_code;
                     $data_initiative_detail_raw['init_id'] = $value->id;
                     $data_initiative_detail_raw['title'] = $value->title;
-                    $data_initiative_detail_raw['kuantitatif_mtd'] = $final_monthly_score;
-                    $data_initiative_detail_raw['kuantitatif_ytd'] = $final_yearly_score;
+                    $data_initiative_detail_raw['kuantitatif_mtd'] = number_format($final_monthly_score, 2);
+                    $data_initiative_detail_raw['kuantitatif_ytd'] = number_format($final_yearly_score, 2);
                         // milestone
                     $data_initiative_detail_raw['completed'] = $completed;
                     $data_initiative_detail_raw['on_track'] = $on_track;
